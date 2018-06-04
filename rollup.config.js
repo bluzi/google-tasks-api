d4924a30d77dd331869b3ef327d7ca70934c74a0
Eliran Pe'er <eliran.peer@forter.com>
2018-06-05 01:10:37 +0300

    add babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,18 @@
 const uglify = require('rollup-plugin-uglify').uglify;
 const minify = require('uglify-es').minify;
+const babel = require('rollup-plugin-babel');
 
 export default {
-    input: 'lib/index.js',
-    output: {
-      file: 'dist/index.min.js',
-      format: 'umd',
-      name: 'googleTasksApi'
-    },
-    plugins: [ uglify({}, minify) ]
-  };
+  input: 'lib/index.js',
+  output: {
+    file: 'dist/index.min.js',
+    format: 'umd',
+    name: 'googleTasksApi'
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    uglify({}, minify),
+  ]
+};
